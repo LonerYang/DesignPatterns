8b1e1ee85e7c97aad0d1a929698ebd0c4146305d
LonerYang <1051233525@qq.com>
2019-06-10 21:33:31 +0800

    添加双重加锁的单例模式为什么需要用volatile修饰的注释

--- src/com/ahead/singleton/Mgr02.java
+++ src/com/ahead/singleton/Mgr02.java
@@ -5,7 +5,18 @@ package com.ahead.singleton;
  *
  */
 public class Mgr02 {
-	
+	/**
+	 * 采用 volatile 关键字修饰也是很有必要的， INSTANCE = new Mgr02(); 这段代码其实是分
+	 * 为三步执行：
+	 * 1. 为 INSTANCE 分配内存空间
+	 * 2. 初始化 INSTANCE
+	 * 3. 将 INSTANCE 指向分配的内存地址
+	 * 但是由于 JVM 具有指令重排的特性，执行顺序有可能变成 1->3->2。指令重排在单线程环境下不会出先问题，但是在
+	 * 多线程环境下会导致一个线程获得还没有初始化的实例。例如，线程 T1 执行了 1 和 3，此时 T2 调用
+	 * getInstance() 后发现 INSTANCE 不为空，因此返回 INSTANCE，但此时 INSTANCE 还未被
+	 * 初始化。
+	 * 使用 volatile 可以禁止 JVM 的指令重排，保证在多线程环境下也能正常运行。
+	 */
 	private volatile static Mgr02 INSTANCE = null;
 
 	public static Mgr02 getInstance() {
